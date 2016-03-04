37756a85247986574e855051806252a2929c89fb
aldopareja <aldo.spam@live.com>
2016-03-04 09:45:02 -0500

    Creo que no hice ningún cambio pero voy a reiniciar el equipo

--- main.py
+++ main.py
@@ -29,26 +29,11 @@ agents.append(agent.agent(1,2,1))
 
 #Experience
 #sensitivity to New information
-delta=0.1
-#getting routes and costs
-routesAndCosts=getRoutes(network, agents, 10, cost=True)
-#calculate the first probability distribution
-probDist=updateProb(routesAndCosts)
-#get a route draw for each agent
-while True:
-    for i in range(3000):
-        Draws=generateDraws(probDist,routesAndCosts['routes'])
-        #run basic algorithm with the draw
-        tempNet=deepcopy(network)
-        tempAgents=deepcopy(agents)
-        transOut=execute(tempNet, tempAgents, Draws['routesDraw'])
-        #update costs and probability distribution
-        for idx, costs in enumerate(routesAndCosts['costs']):
-            newCost=transOut[idx]['timeFlow'][-1]-transOut[idx]['timeFlow'][0]
-            newCostIdx=Draws['routesDrawIdx'][idx]
-            costs[newCostIdx]=1*delta*(newCost)+(1-delta)*costs[newCostIdx]
-        probDist=updateProb(routesAndCosts)
-    print('aca')
+#testing period
+
+
+
+
 #agents routes pool calculation
 agentsRoutesComb=getRoutes(network,agents,10)
 
